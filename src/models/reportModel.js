dae7c8b1ad413984ec7256cac41701f65a6c2d8c
Aditya-170 <pramodkumar3615@gmail.com>
2025-07-31 21:17:45 +0530

    added report model and it's backend code

---
+++ src/models/reportModel.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+
+const reportSchema = new mongoose.Schema({
+  reportId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  attendingPhysician: {
+    type: String,
+    required: true,
+    default: "Dr. Healthcare",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  medicalLicense: {
+    type: String,
+    required: true,
+  },
+  patientName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "other"],
+    default: "male"
+  },
+  bloodType: {
+    type: String,
+    default: "—",
+  },
+  weight: {
+    type: String, // You can change to Number if you always store in KG only
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  symptoms: {
+    type: [String],
+    default: [],
+  },
+  observations: {
+    type: [String],
+    default: [],
+  },
+  prescription: {
+    type: [String],
+    default: [],
+  },
+  remarks: {
+    type: String,
+    default: '',
+  }
+});
+
+const Report = mongoose.model('Report', reportSchema);
+
+module.exports = Report;
